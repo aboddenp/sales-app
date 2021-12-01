77d9ef55eb6d6e3e07ac31b4fbce583639025ce1
Aster Bodden <aster.bodden@agilesolutions.com>
2021-12-01 09:26:18 -0600

    use fixed user list to optimize renders on api data

--- salesfrontend/src/components/UserList.js
+++ salesfrontend/src/components/UserList.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import { useNavigate } from "react-router-dom";
+import { FixedSizeList as List } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
@@ -12,40 +13,48 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 
-function UserList({users}) {
+function UserList({ users }) {
     let navigate = useNavigate();
 
+    let Row = (index, style) => (
+        <Item
+            sx={{
+                display: "flex",
+                ":hover": {
+                    boxShadow: 6,
+                },
+            }}
+            style={style}
+            key={users.username}
+            onClick={() => navigate("/sales/" + users[index].username)}
+        >
+            <h1>test</h1>
+            <Box>
+                <Typography variant="h5">{index}</Typography>
+            </Box>
+            <Box sx={{ ml: 10 }}>
+                <Typography variant="h5">{users[index].username}</Typography>
+            </Box>
+            <Box sx={{ ml: "auto", mr: 2 }}>
+                <Typography variant="h5">{users[index].saleCount}</Typography>
+            </Box>
+        </Item>
+    );
+
     return (
-        <Stack spacing={3} alignItems="stretch">
-            {users.map((user, index) => {
-                return (
-                    <Item
-                        sx={{
-                            display: "flex",
-                            ":hover": {
-                                boxShadow: 6,
-                            },
-                        }}
-                        key={user.username}
-                        onClick={() => navigate("/sales/" + user.username)}
-                    >
-                        <Box>
-                            <Typography variant="h5">{index}</Typography>
-                        </Box>
-                        <Box sx={{ ml: 10 }}>
-                            <Typography variant="h5">
-                                {user.username}
-                            </Typography>
-                        </Box>
-                        <Box sx={{ ml: "auto", mr: 2 }}>
-                            <Typography variant="h5">
-                                {user.saleCount}
-                            </Typography>
-                        </Box>
-                    </Item>
-                );
-            })}
-        </Stack>
+        <AutoSizer>
+            {({ height, width }) => (
+                <List
+                    className="List"
+                    height={height}
+                    itemCount={users.length}
+                    itemSize={50}
+                    width={width}
+                >
+                    {Row}
+                </List>
+            )}
+        </AutoSizer>
     );
 }
 
