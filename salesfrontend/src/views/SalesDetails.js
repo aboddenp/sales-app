e13c74f2d193a76fbb8634ff96ee3b7fb4984f29
Aster Bodden <aster.bodden@agilesolutions.com>
2021-12-07 15:02:48 -0600

    Fix docker compose issue

--- salesfrontend/src/views/SalesDetails.js
+++ salesfrontend/src/views/SalesDetails.js
@@ -1,3 +1,4 @@
+import * as React from "react"
 import { Box, Typography, Skeleton } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { DataGrid } from "@mui/x-data-grid";
